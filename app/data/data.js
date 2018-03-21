2972a03a3d4631d47615e1b0218010a434476ab3
SlaviTod <slavka_todorova@abv.bg>
2018-03-21 13:04:14 +0200

    data module was created. it works with db through sequelize

--- app/data/data.js
+++ app/data/data.js
@@ -1,75 +1,19 @@
-const foods = [{
-        id: 1,
-        name: 'Chicken soup',
-        categoryId: 1,
-        price: 3.50,
-        weight: 250,
-        ingUrl: '',
-    },
-    {
-        id: 2,
-        name: 'Vegetables soup',
-        categoryId: 1,
-        price: 3.00,
-        weight: 300,
-        ingUrl: '',
-    },
-    {
-        id: 3,
-        name: 'Shopska salad',
-        categoryId: 2,
-        price: 4.00,
-        weight: 200,
-        ingUrl: '',
-    },
-    {
-        id: 4,
-        name: 'Verona',
-        categoryId: 3,
-        price: 8.00,
-        weight: 400,
-        ingUrl: '',
-    },
-];
 
-const categories = [{
-        id: 1,
-        name: 'soups'
-    },
-    {
-        id: 2,
-        name: 'salads'
-    },
-    {
-        id: 3,
-        name: 'pizza'
-    },
-    {
-        id: 4,
-        name: 'pasta'
-    },
-    {
-        id: 5,
-        name: 'desearts'
-    },
-];
+const {
+    Food,
+    Category,
+    Order,
+    OrderDetails,
+    User
+} = require('../db/models');
 
-
-const getAllFood = () => {
-    return foods;
-};
-
-const getFoodById = (id) => {
-    return foods.find((f) => f.id === id);
-};
-
-const createFood = (obj) => {
-    
-    return foods.push(obj);
-};
+const Data = require('./generic.data');
+const SuperheroesData = require('./superheroes.data');
 
 module.exports = {
-    getAllFood,
-    getFoodById,
-    createFood,
+    food: new  Data(Food),
+    category: new Data(Category),
+    order: new Data(Order),
+    orderDetails: new Data(OrderDetails),
+    user: new Data(User),
 };
