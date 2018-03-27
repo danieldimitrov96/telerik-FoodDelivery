a191cd176dd3beeae4d7d5d89e6bee3f4130ad52
Daniel Dimitrov <dakata962@gmail.com>
2018-03-27 23:37:52 +0300

    Logged NODE_ENV

--- db/models/index.js
+++ db/models/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log('NODE_ENV: ' + env);
+console.log('-'.repeat(33));
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
